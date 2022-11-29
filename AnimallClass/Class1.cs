68176e532cfeda68d347866ad88fba6f1ab02d0d
Benjamin Hooper <HOO19000635@btc.ac.uk>
2022-11-29 10:29:24 +0000

    Add project files.

---
+++ AnimallClass/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimallClass
+{
+    internal class Animal
+    {
+        //objects of the animal class
+        public string Animal_Type;
+        public string colour;
+        public float weight;
+        public string gender;
+        public Animal(string animal_type, string Colour, float Weight, string Gender )
+        {
+            Animal_Type = animal_type;
+            colour = Colour;
+            weight = Weight;
+            gender = Gender;
+
+        }
+        public override string ToString()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            return $"Animal: {Animal_Type}\nColour: {colour}\nWeight: {weight} KG\nGender: {gender}\n--------------";
+        }
+    }
+}
+
+
+
+
+
+
+
---
+++ AnimallClass/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace AnimallClass
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            //instantiation
+            //Animal One
+            Animal Animal1 = new Animal("Sheep","White", 5f, "Male");
+            //Animal Two
+            Animal Animal2 = new Animal("Cat", "Oranage", 0.5f, "Female");
+            //Animal Three
+            Animal Animal3 = new Animal("Shark", "Grey/White", 30f, "Male");
+            //Animal Four
+            Animal Animal4 = new Animal("Kangaroo", "Brown", 8.23f, "Male");
+
+            List<Animal> animals = new List<Animal>() { Animal1, Animal2, Animal3 };
+
+            foreach (var item in animals)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
